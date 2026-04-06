f718c42ab7a2338b61fd8b2562f300b8736b623d
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-04-06 15:19:30 +0300

    .

--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 500 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const mime = file.mimetype || "";
 
