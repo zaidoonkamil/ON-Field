cc801921c6f212f6a4d4063d8d0f19d8cd4bf98c
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-04-20 12:32:55 +0300

    .

--- routes/Post.js
+++ routes/Post.js
@@ -97,7 +97,7 @@ router.post(
   "/posts",
   authenticateToken,
   requireRoles("admin", "super_admin", "photographer"),
-  uploadPostMedia.array("media", 300),
+  uploadPostMedia.array("media", 600),
   async (req, res) => {
   try {
     const { text } = req.body;
@@ -171,7 +171,7 @@ router.put(
   "/posts/:id",
   authenticateToken,
   requireRoles("admin", "super_admin", "photographer"),
-  uploadPostMedia.array("media", 300),
+  uploadPostMedia.array("media", 600),
   async (req, res) => {
     try {
       const { id } = req.params;
